5f6403eca376a9789b59d522949ec7c9ea0c91c5
KStory34 <34momst0ry@gmail.com>
2025-05-20 02:19:23 -0400

    Update airline11.py

--- airline11.py
+++ airline11.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
-
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
